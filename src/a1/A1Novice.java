5eda4fa1623788a8cecc95113fa29757c46fec78
jackfan <jackfan@cci0pdjgh5aws.wireless-1x.unc.edu>
2019-08-27 18:57:51 -0400

    Trial 1

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -1,14 +1,33 @@
-package a1;
+import java.util.*;
+import java.io.*;
 
-import java.util.Scanner;
-
-public class A1Novice {
-
-	public static void main(String[] args) {
-		
-		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
-		
-	}
+class A1Novice {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        String[][] names = new String[n][2];
+        String[][][] data = new String[n][][];
+        for(int i=0; i<n; i++)
+        {
+            String first = scan.next();
+            String last = scan.next();
+            names[i][0] = first;
+            names[i][1] = last;
+            int num = scan.nextInt();
+            data[i] = new String[num][3];
+            for(int j=0; j<num; j++)
+            {
+                data[i][j][0] = scan.next();
+                data[i][j][1] = scan.next();
+                data[i][j][2] = scan.next();
+            }
+        }
+        for(int i=0; i<data.length; i++) {
+            double price = 0;
+            for (int j = 0; j < data[i].length; j++) {
+                price += Double.parseDouble(data[i][j][0]) * Double.parseDouble(data[i][j][2]);
+            }
+            System.out.println(names[i][0].charAt(0) + ". " + names[i][1] + ": " + String.format("%.2f", price));
+        }
+    }
 }
