431b78955a7baf82eca64ac7bbc88b1d4a6edb41
jackfan <jackfan@152.23.110.125>
2019-09-01 23:23:02 -0400

    s

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -7,14 +7,22 @@ class A1Jedi
   public static void main(String[] args)
   {
     Scanner scan = new Scanner(System.in);
+    //initiate scanner
+    
     int n = scan.nextInt();
+    //reads number of unique items
+    
     String[] order = new String[n];
+    //creates empty array 'order' with index count n
+    
     for(int i=0; i<n; i++)
     {
       String s = scan.next();
       double cost = scan.nextDouble();
       order[i] = s;
     }
+    //fills order array with respective cost
+    
     HashMap<String, Integer> number = new HashMap<String, Integer>();
     HashMap<String, Integer> people = new HashMap<String, Integer>();
     int num_people = scan.nextInt();
@@ -36,6 +44,9 @@ class A1Jedi
           people.put(fruit, people.getOrDefault(fruit,0) + 1);
         }
     }
+    //maps people as key and number of orders as values
+    //iterating through second part of input to add values to keys
+    
     for(int i=0; i<order.length; i++)
     {
       int people_ = people.getOrDefault(order[i], 0);
@@ -48,6 +59,8 @@ class A1Jedi
       {
         System.out.println("No customers bought " + order[i]);
       }
+      //updates hashmap
+      //checks output and outputs desired statement
     }
   }
 }
